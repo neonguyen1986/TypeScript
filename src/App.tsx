aa4ec6002231aeb59d78d5498a9941939d544319
Neo Nguyen <neonguyen.ca@gmail.com>
2023-09-08 00:20:30 -0600

    1. Clone and Separate CV

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import InputAndTable from './examples/InputAndTable'
-import PropsWithTypeScript from './examples/PropsWithTypeScript'
+import Portfolio from './screens/portfolio'
+
 
 function App() {
     const [count, setCount] = useState(0)
 
     return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h2>Vite + React</h2>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <div className="read-the-docs">
-                {/* <InputAndTable /> */}
-                <PropsWithTypeScript
-                    name={'Minh'}
-                    age={38}
-                />
-            </div>
-        </div>
+        <Portfolio />
     )
 } 1
 
